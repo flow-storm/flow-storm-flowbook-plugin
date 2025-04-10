9958f147447d8b876790f8c8e7e9746efd3b65be
Juan Monetta <jpmonettas@gmail.com>
2025-04-10 12:14:46 -0300

    Updating the UI

--- src/flow_storm/plugins/serde/ui.clj
+++ src/flow_storm/plugins/serde/ui.clj
@@ -1,44 +1,67 @@
 (ns flow-storm.plugins.serde.ui
   (:require [flow-storm.debugger.ui.plugins :as fs-plugins]
             [flow-storm.debugger.ui.components :as ui]
+            [flow-storm.debugger.ui.utils :as ui-utils :refer [event-handler]]
             [flow-storm.debugger.state :as dbg-state]
             [flow-storm.debugger.runtime-api :as runtime-api :refer [rt-api]])
-  (:import [javafx.stage FileChooser FileChooser$ExtensionFilter]))
+  (:import [javafx.stage FileChooser FileChooser$ExtensionFilter]
+           [javafx.scene Scene]
+           [javafx.stage Stage]))
+
+(defn- create-notebook-window [notebook-web-view]
+  (try
+    (let [notebook-w 1000
+          notebook-h 600
+          stage (doto (Stage.)
+                  (.setTitle "FlowStorm replay notebook"))
+          scene (Scene. notebook-web-view
+                        notebook-w
+                        notebook-h)]
+
+      (.setScene stage scene)
+
+      (.setOnCloseRequest stage (event-handler [_] (dbg-state/unregister-jfx-stage! stage)))
+      (dbg-state/register-jfx-stage! stage)
+
+      (let [{:keys [x y]} (ui-utils/stage-center-box (dbg-state/main-jfx-stage) notebook-w notebook-h)]
+        (.setX stage x)
+        (.setY stage y))
+
+      (-> stage .show))
+
+    (catch Exception e
+      (.printStackTrace e))))
 
 (defn- on-create [_]
   (try
     (let [flow-clear (fn [_])
 
-          save-pane (ui/v-box
-                     :childs
-                     [(ui/label :text "Save")
-                      (ui/h-box :childs
-                                [(ui/label :text "File:")
-                                 (ui/icon-button :icon-name  "mdi-content-save"
-                                                 :tooltip "Save flow"
-                                                 :on-click (fn []
-                                                             (let [file-chooser (doto (FileChooser.)
-                                                                                  (.setTitle "Save flow"))
-                                                                   _ (-> file-chooser .getExtensionFilters (.addAll [(FileChooser$ExtensionFilter. "Edn Files" (into-array String ["*.edn"]))]))
-                                                                   selected-file (.showSaveDialog file-chooser (dbg-state/main-jfx-stage))
-                                                                   selected-file-path (when selected-file (.getAbsolutePath selected-file))]
-                                                               (when selected-file-path
-                                                                 (runtime-api/call-by-fn-key rt-api :plugins.serde/serialize [selected-file-path #{}])))))])])
-          load-pane (ui/v-box :childs [(ui/label :text "Load")
-                                       (ui/h-box :childs [(ui/label :text "File:")
-                                                          (ui/icon-button :icon-name  "mdi-folder-upload"
-                                                                          :tooltip "Load flow"
-                                                                          :on-click (fn []
-                                                                                      (let [file-chooser (doto (FileChooser.)
-                                                                                                           (.setTitle "Select a file"))
-                                                                                            _ (-> file-chooser .getExtensionFilters (.addAll [(FileChooser$ExtensionFilter. "Edn Files" (into-array String ["*.edn"]))]))
-                                                                                            selected-file (.showOpenDialog file-chooser (dbg-state/main-jfx-stage))
-                                                                                            selected-file-path (when selected-file (.getAbsolutePath selected-file))]
-                                                                                        (when selected-file-path
-                                                                                          (runtime-api/call-by-fn-key rt-api :plugins.serde/replay-timelines [selected-file-path])))))])])
-          main-pane (ui/v-box :childs [save-pane
-                                       load-pane])]
-
+          main-pane (ui/v-box :childs [(ui/button :label "Save flows"
+                                                  :on-click (fn []
+                                                              (let [file-chooser (doto (FileChooser.)
+                                                                                   (.setTitle "Save flow"))
+                                                                    _ (-> file-chooser .getExtensionFilters (.addAll [(FileChooser$ExtensionFilter. "Edn Files" (into-array String ["*.edn"]))]))
+                                                                    selected-file (.showSaveDialog file-chooser (dbg-state/main-jfx-stage))
+                                                                    selected-file-path (when selected-file (.getAbsolutePath selected-file))]
+                                                                (when selected-file-path
+                                                                  (runtime-api/call-by-fn-key rt-api :plugins.serde/serialize [selected-file-path #{}])))))
+                                       (ui/button :label "Load flows"
+                                          :on-click (fn []
+                                                      (let [file-chooser (doto (FileChooser.)
+                                                                           (.setTitle "Select a file"))
+                                                            _ (-> file-chooser .getExtensionFilters (.addAll [(FileChooser$ExtensionFilter. "Edn Files" (into-array String ["*.edn"]))]))
+                                                            selected-file (.showOpenDialog file-chooser (dbg-state/main-jfx-stage))
+                                                            selected-file-path (when selected-file (.getAbsolutePath selected-file))]
+                                                        (when selected-file-path
+                                                          (runtime-api/call-by-fn-key rt-api :plugins.serde/replay-timelines [selected-file-path])))))
+                                       (ui/button :label "Load notebook"
+                                          :on-click (fn []
+                                                      (let [{:keys [web-view set-html set-handlers]} (ui/web-view)]
+                                                        (set-html "<html><body><button onclick='test.invoke(42,44)'>Click me</button></body></html>")
+                                                        (set-handlers {"test" (fn [arg1 arg2] (println "@@@@ we got it" arg1 arg2))})
+                                                        (create-notebook-window web-view))))
+                                       ]
+                              :spacing 10)]
       {:fx/node main-pane
        :flow-clear flow-clear})
     (catch Exception e
